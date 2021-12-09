bc464f727270517bb47b50d1d792403d51302d35
deep1899 <madoo12@live.com>
2021-12-09 16:47:11 +0400

    first commit

---
+++ exercises.py
@@ -0,0 +1,57 @@
+//EXERCISE XP
+//exercise 1
+for i in range(5): print('Hello World')
+
+//exercise 2
+(9**3)*8
+
+//exercise 3
+x=3
+y=5
+y<x (false)
+x==x (true)
+x=="x"(false)
+"x">x (not supported 'str' and 'int')
+"Hello"=="hello" (false)
+
+//exercise 4
+computer_brand = "hp"
+print(computer_brand);
+print("I have a " + computer_brand + " computer.")
+
+//exercise 5
+name = "Deepesh"
+age = 22
+shoe_size = 46
+info= "I am {name}, I am {age} years old and my shoe size is {shoe_size}."
+print(info)
+
+//exercise 6
+a = 100
+b = 10
+if a > b:print("Hello World")
+
+//exercise 7
+number = int(input("Please input a number: "))
+
+if number % 2 == 0: print("The number is even")
+else: print("The number is odd")
+
+//exercise 8
+
+my_name = "Deepesh"
+name = input('Please state your name: ')
+if my_name == name: print("Nice we have the same name!")
+else: print("You have a nice name!")
+
+//exercise 9
+height = int(input("Please input your height: "))
+if height > 145: print("You are tall enough to ride!")
+else: print("You are not tall enough to ride.")
+
+//EXERCISE XP GOLD
+//exercise 1
+for i in range(4): print('Hello world \n I love python')
+
+//exercise 2
+month = int(input("Please input a month: "))
